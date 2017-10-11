04ca92d3c73364887cc7c6751c99e85105f11fa3
shiyan5 <shixi_haiyan5@staff.weibo.com>
2017-10-11 10:56:29 +0800

    hanio

--- src/recursion/dp.java
+++ src/recursion/dp.java
@@ -341,4 +341,21 @@ public class dp {
             dp[i] = l + 1;
         }
     }
+
+    //汉诺塔问题
+    public void hanoi(int n) {
+        if (n > 0) {
+            func(n, "left", "mid", "right");
+        }
+    }
+
+    private void func(int n, String from, String mid, String to) {
+        if (n == 1) {
+            System.out.println("move from " + from + "to" + to);
+        } else {
+            func(n - 1, from, to, mid);
+            func(1, from, mid, to);
+            func(n - 1 ,mid, from, to);
+        }
+    }
 }
